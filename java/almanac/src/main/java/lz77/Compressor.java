0f6908bfd422ecf7a0729fee14e23b2ae656fcdb
Peter Cipov <peter.cipov@iflix.com>
2021-05-21 22:20:23 +0200

    T.C.R.

--- java/almanac/src/main/java/lz77/Compressor.java
+++ java/almanac/src/main/java/lz77/Compressor.java
@@ -1,4 +1,10 @@
 package lz77;
 
+import java.util.List;
+
 public class Compressor {
+
+    public List<LengthDistancePair> compress(String input) {
+        return List.of();
+    }
 }
--- java/almanac/src/test/java/lz77/CompressorTest.java
+++ java/almanac/src/test/java/lz77/CompressorTest.java
@@ -1,7 +1,20 @@
 package lz77;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompressorTest {
 
+    @Test
+    public void compressionWillReturnEmptyListOfLengthDistancePairsIfRecevingEmptyStringAsInput () {
+        Compressor compressor = new Compressor();
+
+        List<LengthDistancePair> pairs = compressor.compress("");
+
+        Assertions.assertTrue(pairs.isEmpty());
+    }
 }
